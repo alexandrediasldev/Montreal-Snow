c7021e1fdb879c1e8d25d80f51c0de023c84a608
Gaëtan Pusel <gaetan.pusel@epita.fr>
2021-05-23 18:46:24 +0200

    first use of osmnx to print a city roadmap

--- demo.py
+++ demo.py
@@ -1,2 +1,26 @@
-#TODO
+import argparse
 
+import osmnx as ox
+
+
+def main():
+    ps = argparse.ArgumentParser(description="Big demo.")
+    ps.add_argument("--city",
+                    type=str,
+                    help="Specify city to search.")
+    ps.add_argument("--country",
+                    type=str,
+                    help="Specify country to search.")
+    args = ps.parse_args()
+    if args.city is not None:
+        if args.country is None:
+            country = "France"
+        else:
+            country = args.country
+
+        city = ox.graph_from_place(args.city + ', ' + country, network_type='drive')
+        ax = ox.plot_graph(city)
+
+
+if __name__ == '__main__':
+    main()
