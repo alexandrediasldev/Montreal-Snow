30bd53425047609300571fbe537832585592e03e
alexandre.dias <alexandre.dias@epita.fr>
2021-05-28 12:27:17 +0200

    added optimal but we get a memory error

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 import argparse
 import real.plot_path as pl
-
+import itertools
+import copy
 import networkx as nx
 import osmnx as ox
 import plotly.graph_objects as go
@@ -22,7 +23,9 @@ def main():
             country = args.country
 
         G = ox.graph_from_place(args.city + ', ' + country, network_type='drive')
+        G = ox.utils_graph.get_undirected(G)
 
+        print(len(G.nodes))
         route = nx.shortest_path(G, list(G.nodes)[0], list(G.nodes)[-1])
 
         long = []
@@ -31,6 +34,11 @@ def main():
             point = G.nodes[i]
             long.append(point['x'])
             lat.append(point['y'])
+        nodes_odd_degree = [v for v, d in G.degree if d % 2 == 1]
+        odd_node_pairs = list(itertools.combinations(nodes_odd_degree, 2))
+
+        print(odd_node_pairs)
+        print('Number of pairs: {}'.format(len(odd_node_pairs)))
 
         origin_point = long[0], lat[0]
         dest_point = long[-1], lat[-1]
@@ -38,7 +46,7 @@ def main():
         print(origin_point)
         print(dest_point)
 
-        pl.plot_path(lat, long, origin_point, dest_point)
+        #pl.plot_path(lat, long, origin_point, dest_point)
 
 
 if __name__ == '__main__':
