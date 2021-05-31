7fda58b124f14b5da03c81e0e2e9d84ae991dfb8
Gaëtan Pusel <gaetan.pusel@epita.fr>
2021-05-31 23:37:39 +0200

    adding Eulerian single detection

--- demo.py
+++ demo.py
@@ -5,6 +5,8 @@ import copy
 import networkx as nx
 import osmnx as ox
 import matplotlib.pyplot as plt
+import pandas as pd
+
 import plotly.graph_objects as go
 
 def parse_argument():
@@ -41,6 +43,53 @@ def create_complete_graph(pair_weights, flip_weights=True):
         wt_i = - v if flip_weights else v
         g.add_edge(k[0], k[1], attr_dict={'distance': v, 'weight': wt_i})
     return g
+def add_augmenting_path_to_graph(graph, min_weight_pairs):
+    """
+    Add the min weight matching edges to the original graph
+    Parameters:
+        graph: NetworkX graph (original graph from trailmap)
+        min_weight_pairs: list[tuples] of node pairs from min weight matching
+    Returns:
+        augmented NetworkX graph
+    """
+
+    # We need to make the augmented graph a MultiGraph so we can add parallel edges
+    graph_aug = nx.MultiGraph(graph.copy())
+    for pair in min_weight_pairs:
+        graph_aug.add_edge(pair[0],
+                           pair[1],
+                           attr_dict={'distance': nx.dijkstra_path_length(graph, pair[0], pair[1]),
+                                      'trail': 'augmented'}
+                          )
+    return graph_aug
+def create_eulerian_circuit(graph_augmented, graph_original, starting_node=None):
+    """Create the eulerian path using only edges from the original graph."""
+    euler_circuit = []
+    naive_circuit = list(nx.eulerian_circuit(graph_augmented, source=starting_node))
+
+    for edge in naive_circuit:
+        edge_data = graph_augmented.get_edge_data(edge[0], edge[1])
+        print(edge_data)
+        if "attr_dict" in edge_data[0] and  edge_data[0]['attr_dict']['trail'] != 'augmented':
+            # If `edge` exists in original graph, grab the edge attributes and add to eulerian circuit.
+            edge_att = graph_original[edge[0]][edge[1]]
+            euler_circuit.append((edge[0], edge[1], edge_att))
+        else:
+            aug_path = nx.shortest_path(graph_original, edge[0], edge[1], weight='distance')
+            aug_path_pairs = list(zip(aug_path[:-1], aug_path[1:]))
+
+            print('Filling in edges for augmented edge: {}'.format(edge))
+            print('Augmenting path: {}'.format(' => '.join(str(aug_path))))
+            print('Augmenting path pairs: {}\n'.format(aug_path_pairs))
+
+            # If `edge` does not exist in original graph, find the shortest path between its nodes and
+            #  add the edge attributes for each link in the shortest path.
+            for edge_aug in aug_path_pairs:
+                edge_aug_att = graph_original[edge_aug[0]][edge_aug[1]]
+                euler_circuit.append((edge_aug[0], edge_aug[1], edge_aug_att))
+
+    return euler_circuit
+
 
 def main():
 
@@ -92,7 +141,102 @@ def main():
     plt.axis('off')
     plt.title('Complete Graph of Odd-degree Nodes')
     plt.show()
+    # Step 2.4: Compute Minimum Weight Matching
+    # Compute min weight matching.
+    # Note: max_weight_matching uses the 'weight' attribute by default as the attribute to maximize.
+    odd_matching_dupes = nx.algorithms.max_weight_matching(g_odd_complete, True)
 
+    print('Number of edges in matching: {}'.format(len(odd_matching_dupes)))
+    print(odd_matching_dupes)
+
+    # Convert matching to list of deduped tuples
+    odd_matching = list(pd.unique([tuple(sorted([k, v])) for k, v in odd_matching_dupes]))
+
+    # Counts
+    print('Number of edges in matching (deduped): {}'.format(len(odd_matching)))
+    print(odd_matching)
+
+    plt.figure(figsize=(8, 6))
+
+    # Plot the complete graph of odd-degree nodes
+    nx.draw(g_odd_complete, pos=node_positions, node_size=20, alpha=0.05)
+
+    # Create a new graph to overlay on g_odd_complete with just the edges from the min weight matching
+    g_odd_complete_min_edges = nx.Graph(odd_matching)
+    nx.draw(g_odd_complete_min_edges, pos=node_positions, node_size=20, edge_color='blue', node_color='red')
+
+    plt.title('Min Weight Matching on Complete Graph')
+    plt.show()
+
+    plt.figure(figsize=(8, 6))
+
+    # Plot the original trail map graph
+    nx.draw(G, pos=node_positions, node_size=20, alpha=0.1, node_color='black')
+
+    # Plot graph to overlay with just the edges from the min weight matching
+    nx.draw(g_odd_complete_min_edges, pos=node_positions, node_size=20, alpha=1, node_color='red', edge_color='blue')
+
+    plt.title('Min Weight Matching on Orginal Graph')
+    plt.show()
+
+    # Create augmented graph: add the min weight matching edges to g
+    g_aug = add_augmenting_path_to_graph(G, odd_matching)
+
+    # Counts
+    print('Number of edges in original graph: {}'.format(len(G.edges())))
+    print('Number of edges in augmented graph: {}'.format(len(g_aug.edges())))
+    print("Augmented")
+    print(g_aug.edges())
+    #3.0: Compute Eulerian Circuit
+    s = g_aug.edges()
+    print(s)
+
+    for s1 in s:
+        source_s = s1[0]
+        break
+    naive_euler_circuit = list(nx.eulerian_circuit(g_aug, source=source_s))
+    print('Length of eulerian circuit: {}'.format(len(naive_euler_circuit)))
+
+    # Create the Eulerian circuit
+    euler_circuit = create_eulerian_circuit(g_aug, G, source_s)
+    print('Length of Eulerian circuit: {}'.format(len(euler_circuit)))
+
+    # Preview first 20 directions of CPP solution
+    for i, edge in enumerate(euler_circuit[0:20]):
+        print(i, edge)
+
+    # Computing some stats
+    for edge in euler_circuit:
+        print(edge[2][0])
+    total_mileage_of_circuit = sum([edge[2][0]['length'] for edge in euler_circuit])
+    total_mileage_on_orig_trail_map = sum(nx.get_edge_attributes(G, 'distance').values())
+    _vcn = pd.value_counts(pd.value_counts([(e[0]) for e in euler_circuit]), sort=False)
+    node_visits = pd.DataFrame({'n_visits': _vcn.index, 'n_nodes': _vcn.values})
+    _vce = pd.value_counts(
+        pd.value_counts([sorted(e)[0] + sorted(e)[1] for e in nx.MultiDiGraph(euler_circuit).edges()]))
+    edge_visits = pd.DataFrame({'n_visits': _vce.index, 'n_edges': _vce.values})
+
+    # Printing stats
+    print('Mileage of circuit: {0:.2f}'.format(total_mileage_of_circuit))
+    print('Mileage on original trail map: {0:.2f}'.format(total_mileage_on_orig_trail_map))
+    print('Mileage retracing edges: {0:.2f}'.format(total_mileage_of_circuit - total_mileage_on_orig_trail_map))
+    if(total_mileage_on_orig_trail_map != 0):
+        percent = ((1 - total_mileage_of_circuit / total_mileage_on_orig_trail_map) * - 100)
+    else:
+        percent = 0
+    print('Percent of mileage retraced: {0:.2f}%\n'.format(percent))
+
+    print('Number of edges in circuit: {}'.format(len(euler_circuit)))
+    print('Number of edges in original graph: {}'.format(len(G.edges())))
+    print('Number of nodes in original graph: {}\n'.format(len(G.nodes())))
+
+    print('Number of edges traversed more than once: {}\n'.format(len(euler_circuit) - len(G.edges())))
+
+    print('Number of times visiting each node:')
+    print(node_visits.to_string(index=False))
+
+    print('\nNumber of times visiting each edge:')
+    print(edge_visits.to_string(index=False))
     #print(odd_node_pairs)
     #print('Number of pairs: {}'.format(len(odd_node_pairs)))
 
